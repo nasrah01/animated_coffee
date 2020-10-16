45f046c46de0b7c7b3fbc65f396523a5c5215d52
nasrah01 <nasrah1@hotmail.co.uk>
2020-10-16 21:10:03 +0100

    first commit

---
+++ animated_coffee/src/js/preloader.js
@@ -0,0 +1,38 @@
+import {gsap} from "gsap";
+
+const load = document.querySelector('.preload');
+
+const text = () => {
+ let content = `<div class="preload_cont"><div class="preload_head">Addis Coffee</div></div>
+                <div class="preload_line"></div>
+                <div class="preload_cont"><div class="preload_text">Good from the ground up</div></div>`;
+
+      load.insertAdjacentHTML("beforeend", content);
+};
+
+const textAnimate = () => {
+   text();
+
+   const reveal = gsap.timeline({delay: .5});
+
+   reveal.from(".preload_line", { scaleX: 0, transformOrigin: "top center" });
+   reveal.from(".preload_head", { duration: 0.75, y: 35 }, "text");
+   reveal.from(".preload_text", { duration: 0.75, y: -35 }, "text");
+   reveal.to(".preload_head, .preload_text, .preload_line", {duration: 2, opacity: 0, ease:"none"}, ">2");
+}; 
+
+export const preloadAnimation = () => {
+   textAnimate();
+   // preloader will ease out and the main page has a sliding reveal 
+   const main = document.querySelector('.main');
+
+   // settimeout for the transition between preload and main page
+   window.addEventListener('load', () => {
+      setTimeout( () => {
+         gsap.timeline()
+         .set(main, {className: 'main'})
+         .to(load, 1, {yPercent: 100, ease: 'Power4.easeInOut'})
+         .set(load, {className: 'hidden'}, '+=1')
+      }, 5000)
+   });
+};
